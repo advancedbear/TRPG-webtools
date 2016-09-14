89ad49fa207fb0bf26b8afbf8f5f2c40c9e16383
advanced_bear <advanced_bear@BEAR-TOKYO>
2016-09-14 15:56:31 +0900

    微修正＋キャラページに手を付けた（どうしようかな）

--- js/trpg.js
+++ js/trpg.js
@@ -16,7 +16,7 @@ $(function() {
 		$("#count").append($("<option>").val(a).text(a+" D 6"));
 	}
 	$("select").val("2");
-
+	
 });
 
 function clr() {
@@ -32,10 +32,12 @@ function dice() {
 	var dice_roll = "";
 	sum_2 = 0;
 	six_count = 0;
+	one_count = 0;
 	for (var c = 0; c < n; c++) {
 		var random = Math.random() * 6;
 		var now = 1 + Math.floor(random);
 		if(now ==6)six_count++;
+		if(now ==1)one_count++;
 		console.log(random);
 		console.log(now);
 		sum = sum + now;
@@ -54,7 +56,8 @@ function dice() {
 	}
 	$("#roll").prepend("<hr>");
 	$("#roll").prepend(dice_roll);
-	if(six_count >= 2) $("#roll").prepend("<font color=\"red\">クリティカル！</font><br />");
+	if(six_count >= 2) $("#roll").prepend("<font color=\"blue\">クリティカル！</font><br />");
+	if(one_count == n) $("#roll").prepend("<font color=\"red\">ファンブル！</font><br />");
 	$("#roll").prepend("<small>" + count + "回目: "+sum_2+"<br /></small>");
 	$("#sum").text("Total = " + sum);
 	count++;
